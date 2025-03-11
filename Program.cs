49ba244e8a99cead9f8872a631d5c75fb46ae002
Danilo Costa <dandan_costa2222@outlook.com>
2025-03-10 21:02:51 -0300

    codigo do exercicio

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Preparar...");
+Thread.Sleep(1500);
+Console.WriteLine("Apnotar...");
+Thread.Sleep(1500);
+Console.WriteLine("FOGO!");
+Console.Beep();
+Console.Beep();
+Console.Beep();
